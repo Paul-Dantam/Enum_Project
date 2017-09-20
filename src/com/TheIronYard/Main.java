14fb733e275a270478ba916272abb5fd1fa857c9
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-19 20:22:05 -0500

    add scanner and switch statement

--- src/com/TheIronYard/Main.java
+++ src/com/TheIronYard/Main.java
@@ -1,8 +1,64 @@
 package com.TheIronYard;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        //add into an array list all the months that follow the given month inclusively
+
+        ArrayList<String> inclusiveFutureMonths = new ArrayList<>();
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter a month");
+
+        String month = sc.nextLine();
+
+        switch (month) {
+            case "January":
+                inclusiveFutureMonths.add(Month.JANUARY.getEnglishName());
+            case "February":
+                inclusiveFutureMonths.add(Month.FEBRUARY.getEnglishName());
+            case "March":
+                inclusiveFutureMonths.add(Month.MARCH.getEnglishName());
+            case "April":
+                inclusiveFutureMonths.add(Month.APRIL.getEnglishName());
+            case "May":
+                inclusiveFutureMonths.add(Month.MAY.getEnglishName());
+            case "June":
+                inclusiveFutureMonths.add(Month.JUNE.getEnglishName());
+            case "July":
+                inclusiveFutureMonths.add(Month.JULY.getEnglishName());
+            case "August":
+                inclusiveFutureMonths.add(Month.AUGUST.getEnglishName());
+            case "September":
+                inclusiveFutureMonths.add(Month.SEPTEMBER.getEnglishName());
+            case "October":
+                inclusiveFutureMonths.add(Month.OCTOBER.getEnglishName());
+            case "November":
+                inclusiveFutureMonths.add(Month.NOVEMBER.getEnglishName());
+            case "December":
+                inclusiveFutureMonths.add(Month.DECEMBER.getEnglishName());
+                break;
+            default:
+                break;
+        }
+
+        if (inclusiveFutureMonths.isEmpty()) {
+            System.out.println("That month doesn't exist, try again!");
+        } else {
+            for (String monthsName : inclusiveFutureMonths) {
+                System.out.println(monthsName);
+            }
+        }
     }
+
+
 }
+
--- src/com/TheIronYard/Month.java
+++ src/com/TheIronYard/Month.java
@@ -1,4 +1,29 @@
 package com.TheIronYard;
 
 public enum Month {
+
+
+    JANUARY("January"),
+    FEBRUARY("February"),
+    MARCH("March"),
+    APRIL("April"),
+    MAY("May"),
+    JUNE("June"),
+    JULY("July"),
+    AUGUST("August"),
+    SEPTEMBER("September"),
+    OCTOBER("October"),
+    NOVEMBER("November"),
+    DECEMBER("December");
+
+
+    private String englishName;
+
+    Month(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
 }
